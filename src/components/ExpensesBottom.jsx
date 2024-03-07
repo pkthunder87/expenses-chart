de26b2c245b9ea629d5b12bcf30f2f0e0f18f466
pkthunder87 <pkthunder87@gmail.com>
2024-03-07 11:13:26 -0800

    Feat: Highlights current day active bar cyan

--- src/components/ExpensesBottom.jsx
+++ src/components/ExpensesBottom.jsx
@@ -3,7 +3,7 @@ import ExpensesTotals from './ExpensesTotals';
 
 function ExpensesBottom() {
   return (
-    <div className="grid h-[31.9rem] w-[33.8rem] rounded-[20px] bg-very-pale-orange px-10 py-7 text-dark-brown">
+    <div className="grid h-[31.9rem] w-[33.8rem] grid-rows-[80%_20%] rounded-[20px] bg-very-pale-orange px-10 py-7 text-dark-brown">
       <ExpensesChart />
       <ExpensesTotals />
     </div>
--- src/components/ExpensesChart.jsx
+++ src/components/ExpensesChart.jsx
@@ -1,49 +1,25 @@
-import { BarChart, Bar, Tooltip, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Tooltip, ResponsiveContainer, XAxis } from 'recharts';
+import dataJSON from './data.json';
 
-const data = [
-  {
-    name: 'mon',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'sun',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+const day = new Date();
+const currentDay = day.getDay();
+const dayOfWeek = {
+  0: 'sun',
+  1: 'mon',
+  2: 'tue',
+  3: 'wed',
+  4: 'thu',
+  5: 'fri',
+  6: 'sat',
+};
+
+// Highlights the current days bar in cyan and others in soft red
+const newData = dataJSON.map((data) => {
+  return {
+    ...data,
+    fill: `${data.day === dayOfWeek[currentDay] ? 'hsl(186, 34%, 60%)' : 'hsl(10, 79%, 65%)'}`,
+  };
+});
 
 export default function ExpensesChart() {
   return (
@@ -51,12 +27,29 @@ export default function ExpensesChart() {
       <h2 className="text-[2rem] font-bold tracking-[.02em]">
         Spending - Last 7 days
       </h2>
-      <ResponsiveContainer width={'100%'} height={200}>
-        <BarChart data={data}>
-          <Bar dataKey="uv" fill="#8884d8" />
+      <ResponsiveContainer
+        className="-ml-[.6rem] text-base "
+        width={'104%'}
+        height={'68%'}
+      >
+        <BarChart
+          data={newData}
+          margin={{ top: 40, right: 0, bottom: 5, left: 0 }}
+        >
+          <XAxis
+            dataKey="day"
+            axisLine={false}
+            tickLine={false}
+            interval={0}
+            tick={{ fill: 'hsl(28, 10%, 53%)' }}
+            dy={5}
+          />
+
+          <Bar dataKey="amount" radius={5} barSize={50} />
           <Tooltip />
         </BarChart>
       </ResponsiveContainer>
+      <hr className="mt-7 border-[1px] border-cream"></hr>
     </div>
   );
 }
