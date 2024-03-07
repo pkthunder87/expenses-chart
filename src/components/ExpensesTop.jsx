be0828e5854570be1f056d2f9ea28dd456b81495
pkthunder87 <pkthunder87@gmail.com>
2024-03-06 21:06:48 -0800

    Feat: Finish static desktop design of ExpensesTop

--- src/components/ExpensesTop.jsx
+++ src/components/ExpensesTop.jsx
@@ -1,9 +1,13 @@
 function ExpensesTop() {
   return (
-    <div className="flex h-[7.8rem] w-[33.8rem] items-center justify-center rounded-[20px] bg-soft-red text-white">
-      <h2>My balance</h2>
-      <h3>$921.48</h3>
-      <img className="" src={`./logo.svg`} alt="Two alternating circles logo" />
+    <div className="grid h-[7.8rem] w-[33.8rem] grid-cols-[80%_20%] grid-rows-2 items-center rounded-[20px] bg-soft-red p-6 pl-8 text-white">
+      <h2 className="-mt-2 text-[1.08rem]">My balance</h2>
+      <img
+        className="row-span-2 ml-[.6rem]"
+        src={`./logo.svg`}
+        alt="Two alternating circles logo"
+      />
+      <h3 className="-mt-2 text-[2rem] font-bold tracking-wide">$921.48</h3>
     </div>
   );
 }
