ced86015f597e01a4e2175b93f6e9ae762292274
pkthunder87 <pkthunder87@gmail.com>
2024-03-09 04:32:35 -0800

    Feat: Finish desktop design for ExpensesTotals

--- src/components/ExpensesTotals.jsx
+++ src/components/ExpensesTotals.jsx
@@ -1,5 +1,15 @@
 function ExpensesTotals() {
-  return <div>Expenses Totals</div>;
+  return (
+    <div className=" -mt-5 grid grid-cols-2 grid-rows-2 items-center justify-center">
+      <h2 className=" text-lg text-medium-brown ">Total this month</h2>
+      <div className="text-lg">&nbsp;</div>
+      <h3 className=" -mt-6 text-5xl font-bold tracking-wide">$478.33</h3>
+      <div className=" -mt-7 text-end text-lg">
+        <p className="mt-[.2rem]  font-bold">+2.4%</p>
+        <p className="  -mt-1 text-medium-brown">from last month</p>
+      </div>
+    </div>
+  );
 }
 
 export default ExpensesTotals;
