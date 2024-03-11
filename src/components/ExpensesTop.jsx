074316fc6c0c50e428852cb732f5eda50d0d123b
pkthunder87 <pkthunder87@gmail.com>
2024-03-11 02:56:30 -0700

    Feat: Finish mobile design for ExpensesTop

--- src/components/ExpensesTop.jsx
+++ src/components/ExpensesTop.jsx
@@ -1,13 +1,15 @@
 function ExpensesTop() {
   return (
-    <div className="grid h-[7.8rem] w-[33.8rem] grid-cols-[80%_20%] grid-rows-2 items-center rounded-[20px] bg-soft-red p-6 pl-8 text-white">
-      <h2 className="-mt-2 text-[1.08rem]">My balance</h2>
+    <div className="grid h-[9rem] w-[33.8rem] grid-cols-[80%_20%] grid-rows-2 items-center rounded-xl bg-soft-red p-6 pl-8 text-white sm:h-[7.8rem] sm:rounded-[20px]">
+      <h2 className="-mt-2 text-2xl sm:text-[1.08rem]">My balance</h2>
       <img
-        className="row-span-2 ml-[.6rem]"
+        className="row-span-2 -ml-3 sm:ml-[.6rem] "
         src={`./logo.svg`}
         alt="Two alternating circles logo"
       />
-      <h3 className="-mt-2 text-[2rem] font-bold tracking-wide">$921.48</h3>
+      <h3 className="-mt-2 text-4xl font-bold tracking-wide sm:text-[2rem]">
+        $921.48
+      </h3>
     </div>
   );
 }
