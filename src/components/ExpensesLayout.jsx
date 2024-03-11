79490b2dde80b1450e0b122f5cefc077ab3e28ed
pkthunder87 <pkthunder87@gmail.com>
2024-03-11 02:28:01 -0700

    Feat: Add small drop shadow to ExpensesTop and bottom

--- src/components/ExpensesLayout.jsx
+++ src/components/ExpensesLayout.jsx
@@ -4,10 +4,10 @@ import ExpensesTop from './ExpensesTop';
 function ExpensesLayout() {
   return (
     <div className="flex flex-col gap-6">
-      <div className="">
+      <div className="drop-shadow-sm">
         <ExpensesTop />
       </div>
-      <div className="">
+      <div className="drop-shadow-sm">
         <ExpensesBottom />
       </div>
     </div>
